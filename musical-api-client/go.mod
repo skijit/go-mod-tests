6dcbf26f3ebfd8eaa8f4adf551d7ff618159d690
skijit <tom.skjei@gmail.com>
2019-06-21 16:39:15 -0400

    updated http call

--- musical-api-client/go.mod
+++ musical-api-client/go.mod
@@ -1,3 +1,11 @@
 module github.com/skijit/musical-api-client
 
 go 1.12
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20190421051319-9d40249d3c2f // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/parnurzeal/gorequest v0.2.15
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+)
--- musical-api-client/request.go
+++ musical-api-client/request.go
@@ -2,8 +2,19 @@ package request
 
 import (
 	"fmt"
+
+	"github.com/parnurzeal/gorequest"
 )
 
 func DoSomething() {
-	fmt.Println("I made it, yo yo yo")
+
+	endpt := "http://localhost:5000/api/v1/submitted-events"
+	// resp, body, errs := gorequest.New().
+	_, body, _ := gorequest.New().
+		Post(endpt).
+		Send(`{"pageSize": 1, "pageNum": -1, "sortField": "submittedDt", "isSortAsc": false, "isPending": true}`).
+		End()
+
+	fmt.Println(body)
+
 }
